ccb995eca756b802746397731bb0d30a5d3ca0a0
teppuus <tkellerman16@gmail.com>
2020-06-30 17:53:13 -0400

    navigation from home to another page and back is working

--- App.js
+++ App.js
@@ -8,24 +8,54 @@
 
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import {StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Button,
+  StatusBar,
+} from 'react-native';
 
 import Heading from './Heading';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={styles.container}>
-        <View style={styles.header}>
-          <Heading />
-        </View>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
 
-        <Text style={styles.menu}>MENU WILL BE HERE</Text>
-       
-        <Text style={styles.footer}>FOOTER WILL BE HERE</Text>
+      <View style={styles.header}>
+        <Heading />
       </View>
+
+      <Text style={styles.menu}>MENU WILL BE HERE</Text>
+
+      <Text style={styles.footer}>FOOTER WILL BE HERE</Text>
+    </View>
+  );
+}
+
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
   );
 }
 
@@ -34,13 +64,13 @@ const Stack = createStackNavigator();
 const App: () => React$Node = () => {
   return (
     <NavigationContainer>
-    <>
-      <StatusBar barStyle="dark-content" />
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-      
-    </>
+      <>
+        <StatusBar barStyle="dark-content" />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </>
     </NavigationContainer>
   );
 };
